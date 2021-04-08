61020d0b9571185040f752ed60d23a761e966f49
Frédéric Boulanger <frederic.boulanger@universite-paris-saclay.fr>
2021-04-08 22:42:40 +0200

    Fixed issues with uid on Linux

--- startFramacNoVNC.ps1
+++ startFramacNoVNC.ps1
@@ -4,6 +4,6 @@ $REPO="fredblgr/"
 $NAME="framac-novnc"
 $TAG="2021"
 $RESOL="1440x900"
-docker run --rm --detach --publish 6080:80 --volume "$(PWD):/workspace:rw" --env "RESOLUTION=${RESOL}" --name "${NAME}-run" "${REPO}${NAME}:${TAG}"
+docker run --rm --detach --publish 6080:80 --volume "${PWD}:/workspace:rw" --env "RESOLUTION=${RESOL}" --name "${NAME}-run" "${REPO}${NAME}:${TAG}"
 Start-Sleep -s 5
 Start http://localhost:6080
--- startFramacNoVNC.sh
+++ startFramacNoVNC.sh
@@ -5,22 +5,46 @@ ARCH=`uname -m`
 TAG=2021
 URL=http://localhost:6080
 
+if [[ -z $SUDO_UID ]]
+then
+  # not in sudo
+  USER_ID=`id -u`
+  USER_NAME=`id -n -u`
+else
+  # in a sudo script
+  USER_ID=${SUDO_UID}
+  USER_NAME=${SUDO_USER}
+fi
+
 # You may add --env USER=<login> --env PASSWORD=<password> to run as a regular user (default is root)
 docker run --rm --detach \
-		  --publish 6080:80 \
-		  --volume ${PWD}:/workspace:rw \
-		  --env RESOLUTION=1200x800 \
-		  --name ${NAME} \
-		  ${REPO}${NAME}:${TAG}
+      --publish 6080:80 \
+      --volume ${PWD}:/workspace:rw \
+      --env USERNAME=${USER_NAME} --env USERID=${USER_ID} \
+      --env RESOLUTION=1400x900 \
+      --name ${NAME} \
+      ${REPO}${NAME}:${TAG}
+
 sleep 5
 
-if command -v open 2>&1 /dev/null
+if [[ -z $SUDO_UID ]]
 then
-  open ${URL}
-elif command -v xdg-open 2>&1 /dev/null
-then
-  xdg-open "${URL}"
+     su ${USER_NAME} -c 'open -a firefox http://localhost:6080' \
+  || su ${USER_NAME} -c 'xdg-open http://localhost:6080' \
+  || echo "Point your web browser at http://localhost:6080"
 else
-  echo "# Point your browser at ${URL}"
-  echo "You may install xdg-utils so that I can do that for you."
+     open -a firefox http://localhost:6080 \
+  || xdg-open http://localhost:6080 \
+  || echo "Point your web browser at http://localhost:6080"
 fi
+
+# if command -v open 2>&1 /dev/null
+# then
+#   open ${URL}
+# elif command -v xdg-open 2>&1 /dev/null
+# then
+#   xdg-open "${URL}"
+# else
+#   echo "# Point your browser at ${URL}"
+#   echo "You may install xdg-utils so that I can do that for you."
+# fi
